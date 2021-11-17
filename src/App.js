eaef6fc3183efac1012daf4c5f982cde708c362c
sdumencic <stella.dumencic@gmail.com>
2021-11-18 00:17:15 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Canvas } from './Canvas'
+import { ClearCanvasButton } from './ClearCanvasButton';
+
+function App() {
+  return (
+    <>
+      <Canvas/>
+      <ClearCanvasButton/>
+    </>
+  );
+}
+
+export default App;
