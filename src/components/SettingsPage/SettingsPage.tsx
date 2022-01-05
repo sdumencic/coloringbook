8bd7875b9dbf551dd818d6149b30b8d4c85cba69
sdumencic <stella.dumencic@gmail.com>
2022-01-05 20:07:29 +0100

    ✨🎨 Back button

--- src/components/SettingsPage/SettingsPage.tsx
+++ src/components/SettingsPage/SettingsPage.tsx
@@ -1,7 +1,8 @@
 import "./SettingsPage.scss";
 import { useState } from "react";
-import { FcSpeaker, FcVoicePresentation } from 'react-icons/fc';
-// import { IoTextOutline } from 'react-icons/io'
+import { FcSpeaker, FcVoicePresentation, FcUndo } from 'react-icons/fc';
+import { IoArrowUndo } from 'react-icons/io5';
+import { Link } from 'react-router-dom';
 
 const BritishFlag = 'images/BritishFlag.png';
 const CroatianFlag = 'images/CroatianFlag.png';
@@ -20,34 +21,41 @@ const SettingsPage = () => {
     const sliderChange = (event: any) => {
         localStorage.setItem('duration', event.target.value);
         //dispatch({ type: SettingsTypes.AnimationDuration, payload: event.target.value });
-      };
+    };
 
-    return (<div className="settings">
-        <h1><FcSpeaker size={100}/></h1>
-        <div className="animation">
-            <input
-                type="range"
-                min="0.1"
-                max="2"
-                step="0.1"
-                value="1"
-                className="slider"
-                onChange={sliderChange}
-            />
+    return (
+        <div className="settings">
+            <h1><FcSpeaker size={100} /></h1>
+            <div className="animation">
+                <input
+                    type="range"
+                    min="0.1"
+                    max="2"
+                    step="0.1"
+                    value="1"
+                    className="slider"
+                    onChange={sliderChange}
+                />
+            </div>
+            <h1><FcVoicePresentation size={100} /></h1>
+            <div className="flex">
+                <div className={`item ${language !== 'hr' ? 'active' : ''}`} onClick={() => flagClicked('hr')}>
+                    <img src={CroatianFlag} />
+                </div>
+                <div className={`item ${language !== 'en' ? 'active' : ''}`} onClick={() => flagClicked('en')}>
+                    <img src={BritishFlag} />
+                </div>
+                <div className={`item ${language !== 'de' ? 'active' : ''}`} onClick={() => flagClicked('de')}>
+                    <img src={GermanFlag} />
+                </div>
+            </div>
+            <Link to="/">
+                <div className="hud">
+                    <FcUndo size={30} className="hud-icon" />
+                </div>
+            </Link>
         </div>
-        <h1><FcVoicePresentation size={100}/></h1>
-        <div className="flex">            
-            <div className={`item ${language !== 'hr' ? 'active' : ''}`} onClick={() => flagClicked('hr')}>
-                <img src={CroatianFlag} />
-            </div>
-            <div className={`item ${language !== 'en' ? 'active' : ''}`} onClick={() => flagClicked('en')}>
-                <img src={BritishFlag} />
-            </div>
-            <div className={`item ${language !== 'de' ? 'active' : ''}`} onClick={() => flagClicked('de')}>
-                <img src={GermanFlag} />
-            </div>
-        </div>
-    </div>)
+        )
 }
 
 export default SettingsPage;
