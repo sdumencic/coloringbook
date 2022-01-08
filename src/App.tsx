44accecb24bec91f7295b3d3d01a8d0f87e8731e
Ivan Jandrić <ijandric97@gmail.com>
2022-01-08 14:42:09 +0100

    🐛 Fixed missing client resize listener

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,40 @@ import "./App.scss";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import { ClientTypes } from "./redux/reducers/ClientReducer";
 import ErrorPage from "./components/ErrorPage/ErrorPage";
 import GamePage from "./components/GamePage/GamePage";
 import GameSelectPage from "./components/GameSelectPage/GameSelectPage";
 import HomePage from "./components/HomePage/HomePage";
 import SettingsPage from "./components/SettingsPage/SettingsPage";
+import { debounce } from "./util/misc";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
 
 const App = () => {
+	const dispatch = useDispatch();
+
+	// On window resize, update the dimensions in global state
+	useEffect(() => {
+		const handleResize = () => {
+			dispatch({
+				type: ClientTypes.Resize,
+				payload: {
+					height: window.innerHeight,
+					width: window.innerWidth,
+				},
+			});
+		};
+
+		window.addEventListener("resize", debounce(handleResize, 100));
+
+		return () =>
+			window.removeEventListener("resize", debounce(handleResize, 100));
+		// We want this to run only on mount and unmount, linter cant detect
+		// this use case, so we will disable it :)
+		// eslint-disable-next-line
+	}, []);
+
 	return (
 		<BrowserRouter>
 			<Routes>
