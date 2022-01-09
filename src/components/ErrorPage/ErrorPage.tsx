fc4017e073106fdd97ee53c3f1da0d34e2cc9a8f
sdumencic <stella.dumencic@gmail.com>
2022-01-09 11:01:39 +0100

    🌎 Added translations for error page

--- src/components/ErrorPage/ErrorPage.tsx
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -3,18 +3,25 @@ import "./ErrorPage.scss";
 import { FcUndo } from "react-icons/fc";
 import { FiSettings } from "react-icons/fi";
 import FloatingButton from "../Shared/FloatingButton/FloatingButton";
+import { GlobalState } from "../../redux/store";
 import { Link } from "react-router-dom";
+import { strings } from "../../util/language";
+import { useSelector } from "react-redux";
 
 const IMAGE = "/images/220108backgroundlama.png";
 
 const ErrorPage = () => {
+	const { language } = useSelector((state: GlobalState) => state.settings);
+
 	const style = {
 		backgroundImage: `url(${IMAGE})`,
 	};
 
 	return (
 		<div className="loginBackground" style={style}>
-			<h1 className="error-page-title">Page not found</h1>
+			<h1 className="error-page-title">
+				{strings[language].homePage.altImgRabbit}
+			</h1>
 
 			<Link to="/">
 				<FloatingButton
