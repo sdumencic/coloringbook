63e82b37a5a782afe730fbdfefa147f392befa1b
Ivan Jandrić <ijandric97@gmail.com>
2022-01-16 02:11:32 +0100

    🐛 Toggle draw mode switch now works.

--- src/components/SettingsPage/SettingsPage.tsx
+++ src/components/SettingsPage/SettingsPage.tsx
@@ -1,15 +1,17 @@
 import "./SettingsPage.scss";
 
 import { FcSpeaker, FcUndo, FcVoicePresentation } from "react-icons/fc";
-import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
+import { BsBrush } from "react-icons/bs";
+import { ChangeEvent } from "react";
 import FloatingButton from "../Shared/FloatingButton/FloatingButton";
 import { GlobalState } from "../../redux/store";
-import { IoArrowUndo } from "react-icons/io5";
+import { HiOutlineCursorClick } from "react-icons/hi";
 import { SettingsTypes } from "../../redux/reducers/SettingsReducer";
+import Switch from "./Switch/Switch";
 import { strings } from "../../util/language";
-import Switch from "./switch/Switch";
+import { useNavigate } from "react-router-dom";
 
 const BritishFlag = "images/BritishFlag.png";
 const CroatianFlag = "images/CroatianFlag.png";
@@ -18,12 +20,8 @@ const GermanFlag = "images/GermanFlag.png";
 const SettingsPage = () => {
 	const navigate = useNavigate();
 	const dispatch = useDispatch();
-	const { sound_volume, language } = useSelector(
-		(state: GlobalState) => state.settings
-	);
-	const locationCount = useSelector(
-		(state: GlobalState) => state.actions.locationCount
-	);
+	const { sound_volume, language, draw_mode } = useSelector((state: GlobalState) => state.settings);
+	const locationCount = useSelector((state: GlobalState) => state.actions.locationCount);
 
 	const flagClicked = (country: string) => {
 		if (country !== language) {
@@ -31,14 +29,24 @@ const SettingsPage = () => {
 		}
 	};
 
-	const sliderChange = (event: any) => {
+	const volumeChange = (event: ChangeEvent<HTMLInputElement>) => {
 		dispatch({ type: SettingsTypes.SoundVolume, payload: event.target.value });
+	};
+
+	const drawModeChange = (event: ChangeEvent<HTMLInputElement>) => {
+		dispatch({ type: SettingsTypes.DrawMode, payload: event.target.checked ? "hold" : "toggle" });
 	};
 
 	return (
 		<div className="settings">
 			<h2>{strings[language].settingsPage.mode}</h2>
-			<Switch />
+			<Switch
+				checked={draw_mode === "hold"}
+				onChange={drawModeChange}
+				leftIcon={<HiOutlineCursorClick size={30} />}
+				rightIcon={<BsBrush size={30} />}
+			/>
+
 			<h1>
 				<FcSpeaker size={70} />
 			</h1>
@@ -51,36 +59,29 @@ const SettingsPage = () => {
 					step="1"
 					value={sound_volume}
 					className="slider"
-					onChange={sliderChange}
+					onChange={volumeChange}
 				/>
 			</div>
 
 			<h1>
 				<FcVoicePresentation size={70} />
 			</h1>
+
 			<div className="flex">
-				<div
-					className={`item ${language !== "hr" ? "" : "active"}`}
-					onClick={() => flagClicked("hr")}
-				>
+				<div className={`item ${language !== "hr" ? "" : "active"}`} onClick={() => flagClicked("hr")}>
 					<img src={CroatianFlag} alt={strings[language].settingsPage.hr} />
 					<h2>{strings[language].settingsPage.hr}</h2>
 				</div>
-				<div
-					className={`item ${language !== "en" ? "" : "active"}`}
-					onClick={() => flagClicked("en")}
-				>
+				<div className={`item ${language !== "en" ? "" : "active"}`} onClick={() => flagClicked("en")}>
 					<img src={BritishFlag} alt={strings[language].settingsPage.en} />
 					<h2>{strings[language].settingsPage.en}</h2>
 				</div>
-				<div
-					className={`item ${language !== "de" ? "" : "active"}`}
-					onClick={() => flagClicked("de")}
-				>
+				<div className={`item ${language !== "de" ? "" : "active"}`} onClick={() => flagClicked("de")}>
 					<img src={GermanFlag} alt={strings[language].settingsPage.de} />
 					<h2>{strings[language].settingsPage.de}</h2>
 				</div>
 			</div>
+
 			<FloatingButton
 				icon={<FcUndo size={30} className="floating-button-icon" />}
 				style={{ top: "10px", left: "10px" }}
